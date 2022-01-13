560d1f3bf235d06368d23dccee6a201b22879b17
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-01-13 16:28:01 -0300

    Save score

---
+++ backend/src/main/java/com/devsuperior/dsmovie/services/ScoreService.java
@@ -0,0 +1,64 @@
+package com.devsuperior.dsmovie.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.dto.ScoreDTO;
+import com.devsuperior.dsmovie.entities.Movie;
+import com.devsuperior.dsmovie.entities.Score;
+import com.devsuperior.dsmovie.entities.User;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.repositories.ScoreRepository;
+import com.devsuperior.dsmovie.repositories.UserRepository;
+
+@Service
+public class ScoreService {
+
+	@Autowired
+	private MovieRepository movieRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private ScoreRepository scoreRepository;
+	
+	@Transactional
+	public MovieDTO saveScore(ScoreDTO dto) {
+		
+		User user = userRepository.findByEmail(dto.getEmail());
+		if (user == null) {
+			user = new User();
+			user.setEmail(dto.getEmail());
+			user = userRepository.saveAndFlush(user);
+		}
+				
+		Movie movie = movieRepository.findById(dto.getMovieId()).get();
+		// Recuperar usuário do banco de dados pelo email. Se o usuário não existir, insira no banco.
+		
+		Score score = new Score();
+		score.setMovie(movie);
+		score.setUser(user);
+		score.setValue(dto.getScore());
+		
+		score = scoreRepository.saveAndFlush(score);
+		// Salvar a avaliação do usuário para o dado filme.
+		
+		double sum = 0.0;
+		for (Score s : movie.getScores()) {
+			sum = sum + s.getValue();
+		}
+		
+		double avg = sum / movie.getScores().size();
+		
+		movie.setScore(avg);
+		movie.setCount(movie.getScores().size());
+		
+		movie = movieRepository.save(movie);
+		// Recalcular a avaliação média do filme e salvar no banco de dados.
+		
+		return new MovieDTO(movie);
+	}
+}
