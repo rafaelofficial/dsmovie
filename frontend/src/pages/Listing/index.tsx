082fa1dd2938c998f83851057fdd25246881d80b
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-01-15 04:32:58 -0300

    First request, Fix: add comment

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -5,6 +5,7 @@ import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
+    // FORMA ERRADA
     axios.get(`${BASE_URL}/movies?size=12&page=0`)
         .then(response => {
             console.log(response.data);
